2b102815d7ac59adf07bc682366491cd7affa49f
Igor Boichuk <ihor.boichuk@gmail.com>
2023-01-12 16:57:49 +0200

    Update index.js

--- my_practice/index.js
+++ my_practice/index.js
@@ -458,7 +458,7 @@
 //  повертає масив ключів його власних властивостей.Якщо в об'єкті немає
 //  властивостей, метод поверне порожній масив
 
-// Завдання 14/41
+// Завдання 14/41  --------------------------------------------------------------
 //
 
 // Виконай рефакторинг функції countProps(object), використовуючи метод Object.keys() і,
@@ -471,18 +471,182 @@
 // Функція підраховує тільки власні властивості об'єкта
 // Функція використовує метод Object.keys() і, можливо, цикл for...of
 
-function countProps(object) {
-  object = { mail: "poly@mail.com", isOnline: true, score: 500 };
-  // Change code below this line
-  let propCount = 0;
+// function countProps(object) {
+//   object = { mail: "poly@mail.com", isOnline: true, score: 500 };
+//   // Change code below this line
 
-  for (const key in object) {
-    if (object.hasOwnProperty(key)) {
-      propCount += 1;
-    }
-  }
-  console.log(propCount);
-  return propCount;
-  // Change code above this line
-}
-countProps();
+//   const propCount = Object.keys(object);
+
+//   // for (const key of propCount) {
+//   //   if (object.hasOwnProperty(key)) {
+//   //     propCount += 1;
+//   //   }
+//   // }
+//   // console.log(itemsObject);
+//   // console.log(propCount);
+//   console.log(propCount.length);
+//   return propCount.length;
+//   // Change code above this line
+// }
+// countProps();
+//
+// -------------------------------------------------------------------------------
+
+// Напиши функцію countTotalSalary(salaries), яка приймає об'єкт
+// зарплат, де ім'я властивості - це ім'я співробітника, а значення
+// властивості - це зарплата. Функція повинна розрахувати загальну
+// суму зарплат співробітників і повернути її. Використовуй змінну
+// totalSalary для зберігання загальної суми зарплати.
+
+// Оголошена функція countTotalSalary(salaries)
+// Виклик countTotalSalary({}) повертає 0
+// Виклик countTotalSalary({ mango: 100, poly: 150, alfred: 80 }) повертає 330
+// Виклик countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }) повертає 400
+// Функція враховує тільки власні властивості об'єкта
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   salaries = { mango: 100, poly: 150, alfred: 80 };
+//   // Change code below this line
+//   for (const item of Object.values(salaries)) {
+//     totalSalary += item;
+//   }
+
+//   console.log(Object.values(salaries));
+
+//   console.log(totalSalary);
+//   // Change code above this line
+//   return totalSalary;
+// }
+// countTotalSalary();
+
+// Перебери масив об'єктів colors, використовуючи цикл for...of.
+// Додай у масив hexColors значення властивостей hex, а
+// в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+//   console.log(hexColors);
+//   console.log(rgbColors);
+// }
+
+//
+
+// Напиши функцію getProductPrice(productName), яка приймає один
+// параметр productName - назва продукту. Функція шукає об'єкт продукту
+//  з таким ім'ям (властивість name) в масиві products і повертає його
+//  ціну (властивість price). Якщо продукт з такою назвою не знайдений,
+//   функція повинна повертати null.
+
+// Оголошена функція getProductPrice(productName).
+// Виклик getProductPrice("Radar") повертає 1300.
+// Виклик getProductPrice("Grip") повертає 1200.
+// Виклик getProductPrice("Scanner") повертає 2700.
+// Виклик getProductPrice("Droid") повертає 400.
+// Виклик getProductPrice("Engine") повертає null.
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   productName = "Droid";
+//   for (const item of products) {
+//     // console.log(item);
+//     // console.log(item.name);
+//     // console.log(item.price);
+//     if (item.name === productName) {
+//       console.log(item.price);
+//       return item.price;
+//     }
+//   }
+//   console.log(null);
+//   return null;
+//   // Change code above this line
+// }
+// getProductPrice();
+
+// Напиши функцію getAllPropValues(propName), яка приймає один
+// параметр propName - ім'я (ключ) властивості. Функція повинна
+// повернути масив всіх значень властивості з таким ім'ям з кожного
+// об'єкта в масиві products. Якщо в об'єктах відсутні властивості з
+// таким ім'ям, функція повинна повернути порожній масив.
+
+// Оголошена функція getAllPropValues(propName)
+// Виклик getAllPropValues("name") повертає ["Radar", "Scanner", "Droid", "Grip"]
+// Виклик getAllPropValues("quantity") повертає [4, 3, 7, 9]
+// Виклик getAllPropValues("price") повертає [1300, 2700, 400, 1200]
+// Виклик getAllPropValues("category") повертає []
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   let arr = [];
+//   for (const item of products) {
+//     if (Object.keys(item).includes(propName)) {
+//       arr.push(item[propName]);
+//     }
+//   }
+//   console.log(arr);
+//   return arr;
+//   // Change code above this line
+// }
+// getAllPropValues("quantity");
+//
+//
+//
+//
+//
+// Напиши функцію calculateTotalPrice(productName), яка приймає
+// один параметр productName - назва товару. Функція повинна
+// повернути загальну вартість (ціна * кількість) товару з таким
+// ім'ям з масиву products.
+
+// Оголошена функція calculateTotalPrice(productName)
+// Виклик calculateTotalPrice("Blaster") повертає 0
+// Виклик calculateTotalPrice("Radar") повертає 5200
+// Виклик calculateTotalPrice("Droid") повертає 2800
+// Виклик calculateTotalPrice("Grip") повертає 10800
+// Виклик calculateTotalPrice("Scanner") повертає 8100
+
+// //
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price * product.quantity;
+//     }
+//   }
+//   return 0;
+
+//   // Change code above this line
+// }
+// console.log(calculateTotalPrice("Scanner"));
