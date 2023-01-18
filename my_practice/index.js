29b4e63c8e3b1aa307cf0e8e07a3d92f317e4bd9
Igor Boichuk <ihor.boichuk@gmail.com>
2023-01-18 22:34:18 +0200

    Update index.js

--- my_practice/index.js
+++ my_practice/index.js
@@ -3094,11 +3094,141 @@
 //
 
 // Change code below this line
-const sortByName = (users) => {
-  return [...users].sort((a, b) => a.name.localeCompare(b.name));
-};
+// const sortByName = (users) => {
+//   return [...users].sort((a, b) => a.name.localeCompare(b.name));
+// };
+// console.log(
+//   sortByName([
+//     {
+//       name: "Moore Hensley",
+//       email: "moorehensley@indexia.com",
+//       eyeColor: "blue",
+//       friends: ["Sharron Pace"],
+//       isActive: false,
+//       balance: 2811,
+//       gender: "male",
+//     },
+//     {
+//       name: "Sharlene Bush",
+//       email: "sharlenebush@tubesys.com",
+//       eyeColor: "blue",
+//       friends: ["Briana Decker", "Sharron Pace"],
+//       isActive: true,
+//       balance: 3821,
+//       gender: "female",
+//     },
+//     {
+//       name: "Ross Vazquez",
+//       email: "rossvazquez@xinware.com",
+//       eyeColor: "green",
+//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//       isActive: false,
+//       balance: 3793,
+//       gender: "male",
+//     },
+//     {
+//       name: "Elma Head",
+//       email: "elmahead@omatom.com",
+//       eyeColor: "green",
+//       friends: ["Goldie Gentry", "Aisha Tran"],
+//       isActive: true,
+//       balance: 2278,
+//       gender: "female",
+//     },
+//     {
+//       name: "Carey Barr",
+//       email: "careybarr@nurali.com",
+//       eyeColor: "blue",
+//       friends: ["Jordan Sampson", "Eddie Strong"],
+//       isActive: true,
+//       balance: 3951,
+//       gender: "male",
+//     },
+//     {
+//       name: "Blackburn Dotson",
+//       email: "blackburndotson@furnigeer.com",
+//       eyeColor: "brown",
+//       friends: ["Jacklyn Lucas", "Linda Chapman"],
+//       isActive: false,
+//       balance: 1498,
+//       gender: "male",
+//     },
+//     {
+//       name: "Sheree Anthony",
+//       email: "shereeanthony@kog.com",
+//       eyeColor: "brown",
+//       friends: ["Goldie Gentry", "Briana Decker"],
+//       isActive: true,
+//       balance: 2764,
+//       gender: "female",
+//     },
+//   ])
+// );
+
+//
+
+//
+
+//
+
+//
+
+// Завдання 45/48
+// Доповни код таким чином, щоб у змінній names вийшов масив імен
+// авторів в алфавітному порядку, рейтинг книг яких більший за значення
+// змінної MIN_BOOK_RATING.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+
+// const names = books
+//   .filter((book) => book.rating > MIN_BOOK_RATING)
+//   .map((book) => book.author)
+//   .sort((a, b) => a.localeCompare(b));
+
+// console.log(names);
+
+//
+
+//
+
+//
+
+//
+// Завдання 46 / 48
+
+// Доповни функцію getNamesSortedByFriendCount(users) таким чином,
+// щоб вона повертала масив імен користувачів, відсортований за зростанням
+// кількості їхніх друзів(властивість friends).
+
+// Change code below this line
+const getNamesSortedByFriendCount = (users) => {};
+// Change code above this line
 console.log(
-  sortByName([
+  getNamesSortedByFriendCount([
     {
       name: "Moore Hensley",
       email: "moorehensley@indexia.com",
@@ -3139,7 +3269,7 @@ console.log(
       name: "Carey Barr",
       email: "careybarr@nurali.com",
       eyeColor: "blue",
-      friends: ["Jordan Sampson", "Eddie Strong"],
+      friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
       isActive: true,
       balance: 3951,
       gender: "male",
@@ -3148,7 +3278,12 @@ console.log(
       name: "Blackburn Dotson",
       email: "blackburndotson@furnigeer.com",
       eyeColor: "brown",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      friends: [
+        "Jacklyn Lucas",
+        "Linda Chapman",
+        "Adrian Cross",
+        "Solomon Fokes",
+      ],
       isActive: false,
       balance: 1498,
       gender: "male",
