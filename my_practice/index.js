3415972e90ac52b9431f1b9fc7e9ad0eb914c2f2
Igor Boichuk <ihor.boichuk@gmail.com>
2023-01-18 21:36:31 +0200

    Update index.js

--- my_practice/index.js
+++ my_practice/index.js
@@ -2861,44 +2861,56 @@
 // У змінній sortedByDescentingRating вийшов масив книг,
 //     відсортований за спаданням рейтингу.
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  {
-    title: "Redder Than Blood",
-    author: "Tanith Lee",
-    rating: 7.94,
-  },
-  {
-    title: "Enemy of God",
-    author: "Bernard Cornwell",
-    rating: 8.67,
-  },
-];
-// Change code below this line
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     rating: 7.94,
+//   },
+//   {
+//     title: "Enemy of God",
+//     author: "Bernard Cornwell",
+//     rating: 8.67,
+//   },
+// ];
+// // Change code below this line
 
-const sortedByAuthorName = books;
+// const sortedByAuthorName = [...books].sort((a, b) =>
+//   a.author.localeCompare(b.author)
+// );
+// const sortedByReversedAuthorName = [...books].sort((a, b) =>
+//   b.author.localeCompare(a.author)
+// );
 
-const sortedByReversedAuthorName = books;
+// const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
 
-const sortedByAscendingRating = books;
+// const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
 
-const sortedByDescentingRating = books;
+// console.log(sortedByAuthorName);
+// console.log(sortedByReversedAuthorName);
+// console.log(sortedByAscendingRating);
+// console.log(sortedByDescentingRating);
+//
 
-console.log(sortedByAuthorName);
-console.log(sortedByReversedAuthorName);
-console.log(sortedByAscendingRating);
-console.log(sortedByDescentingRating);
+//
+
+//
+
+//
+
+//
