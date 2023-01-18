897d903000d469f8376f5dc37ea6dfa5f4adae3a
Igor Boichuk <ihor.boichuk@gmail.com>
2023-01-18 21:56:35 +0200

    Update index.js

--- my_practice/index.js
+++ my_practice/index.js
@@ -2921,11 +2921,104 @@
 // їх балансу(властивість balance).
 
 // Change code below this line
-const sortByAscendingBalance = (users) => {};
-// Change code above this line
+// const sortByAscendingBalance = (users) => {
+//   const sortBalance = [...users].sort((a, b) => a.balance - b.balance);
+//   return sortBalance;
+// };
+// // Change code above this line
 
+// console.log(
+//   sortByAscendingBalance([
+//     {
+//       name: "Moore Hensley",
+//       email: "moorehensley@indexia.com",
+//       eyeColor: "blue",
+//       friends: ["Sharron Pace"],
+//       isActive: false,
+//       balance: 2811,
+//       gender: "male",
+//     },
+//     {
+//       name: "Sharlene Bush",
+//       email: "sharlenebush@tubesys.com",
+//       eyeColor: "blue",
+//       friends: ["Briana Decker", "Sharron Pace"],
+//       isActive: true,
+//       balance: 3821,
+//       gender: "female",
+//     },
+//     {
+//       name: "Ross Vazquez",
+//       email: "rossvazquez@xinware.com",
+//       eyeColor: "green",
+//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//       isActive: false,
+//       balance: 3793,
+//       gender: "male",
+//     },
+//     {
+//       name: "Elma Head",
+//       email: "elmahead@omatom.com",
+//       eyeColor: "green",
+//       friends: ["Goldie Gentry", "Aisha Tran"],
+//       isActive: true,
+//       balance: 2278,
+//       gender: "female",
+//     },
+//     {
+//       name: "Carey Barr",
+//       email: "careybarr@nurali.com",
+//       eyeColor: "blue",
+//       friends: ["Jordan Sampson", "Eddie Strong"],
+//       isActive: true,
+//       balance: 3951,
+//       gender: "male",
+//     },
+//     {
+//       name: "Blackburn Dotson",
+//       email: "blackburndotson@furnigeer.com",
+//       eyeColor: "brown",
+//       friends: ["Jacklyn Lucas", "Linda Chapman"],
+//       isActive: false,
+//       balance: 1498,
+//       gender: "male",
+//     },
+//     {
+//       name: "Sheree Anthony",
+//       email: "shereeanthony@kog.com",
+//       eyeColor: "brown",
+//       friends: ["Goldie Gentry", "Briana Decker"],
+//       isActive: true,
+//       balance: 2764,
+//       gender: "female",
+//     },
+//   ])
+// );
+//
+
+//
+
+//
+
+//
+
+//
+// Завдання 43/48
+
+// Доповни функцію sortByDescendingFriendCount(users) таким чином,
+// щоб вона повертала масив користувачів, відсортований за спаданням
+// кількості їхніх друзів(властивість friends).
+
+// Change code below this line
+const sortByDescendingFriendCount = (users) => {
+  const sortByFriendsCount = [...users].sort(
+    (a, b) => b.friends.length - a.friends.length
+  );
+  return sortByFriendsCount;
+};
+// Change code above this line
 console.log(
-  sortByAscendingBalance([
+  sortByDescendingFriendCount([
     {
       name: "Moore Hensley",
       email: "moorehensley@indexia.com",
