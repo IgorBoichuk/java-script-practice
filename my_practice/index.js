daa7281b61caf2e59cedd3c9e15ca39fd9167b84
Igor Boichuk <ihor.boichuk@gmail.com>
2023-01-19 21:33:19 +0200

    Update index.js

--- my_practice/index.js
+++ my_practice/index.js
@@ -3591,22 +3591,42 @@
 // В результаті виклику new Storage(["Nanitoids", "Prolonger", "Antigravitator"]) значення
 // змінної storage - це об'єкт
 
-class Storage {
-  getItems() {
-    return this.items;
-  }
-  addItem(newItem) {
-    this.items = newItem;
-  }
-  removeItem(itemToRemove) {}
-}
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   }
+//   removeItem(itemToRemove) {
+//     this.items.splice(this.items.indexOf(itemToRemove), 1);
+//   }
+// }
 
-// Change code above this line
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-console.log(storage);
+// console.log(storage);
+
+//
+
+//
+
+//
+
+//
+//
+
+//
+
+//
+
+//
